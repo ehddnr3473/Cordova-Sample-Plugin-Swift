6b619d451474b2cd028f62888d44fbb257389069
dong wook kim <ehddnr73@naver.com>
2023-05-25 16:49:44 +0900

    Javascript module code modified.

--- CordovaSamplePluginSwift/www/CordovaSamplePluginSwift.js
+++ CordovaSamplePluginSwift/www/CordovaSamplePluginSwift.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'CordovaSamplePluginSwift', 'coolMethod', [arg0]);
+exports.greet = function(success, error) {
+    exec(success, error, "CordovaSamplePluginSwift", "greet");
+};
+
+exports.presentModalView = function(success, error) {
+    exec(success, error, "CordovaSamplePluginSwift", "presentModalView");
 };
