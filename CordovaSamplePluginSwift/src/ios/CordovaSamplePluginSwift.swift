fd26f8747644eecdb5e1bdecfca2db26c81ffacc
dong wook kim <ehddnr73@naver.com>
2023-05-25 10:16:01 +0900

    Native view 띄우는 메서드 추가

--- CordovaSamplePluginSwift/src/ios/CordovaSamplePluginSwift.swift
+++ CordovaSamplePluginSwift/src/ios/CordovaSamplePluginSwift.swift
@@ -5,11 +5,34 @@
 //  Created by 김동욱 on 2023/05/25.
 //
 
-@objc class CordovaSamplePluginSwift: CDVPlugin {
+@objc(CordovaSamplePluginSwift)
+class CordovaSamplePluginSwift: CDVPlugin {
     @objc(greet:)
     func greet(command: CDVInvokedUrlCommand) {
         print("Received successfully.")
         let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Hi, I'm a Yeolmok on iOS platform")
         self.commandDelegate.send(result, callbackId: command.callbackId)
+    }
+    
+    @objc(presentModalView:)
+    func presentModalView(_ command: CDVInvokedUrlCommand) {
+        print("presentModalView: called.")
+        let viewController = UIViewController()
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello world!"
+        label.font = .boldSystemFont(ofSize: 25)
+        viewController.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: viewController.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: viewController.view.centerYAnchor)
+        ])
+        
+        viewController.view.backgroundColor = .systemPink
+        viewController.modalPresentationStyle = .popover
+        self.viewController.present(viewController, animated: true) { [weak self] in
+            let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "ModalView came up.")
+            self?.commandDelegate.send(result, callbackId: command.callbackId)
+        }
     }
 }
